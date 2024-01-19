30c62d976fb6cd9943a23565c0caff62911749c2
nulld <krawdev@gmail.com>
2024-01-19 11:24:51 -0300

    feat: entities Person and Adress created.

---
+++ src/main/java/com/luckraw/springbootmongodb/model/entity/Address.java
@@ -0,0 +1,11 @@
+package com.luckraw.springbootmongodb.model.entity;
+
+import lombok.Data;
+
+@Data
+public class Address {
+
+    private String address1;
+    private String address2;
+    private String city;
+}
