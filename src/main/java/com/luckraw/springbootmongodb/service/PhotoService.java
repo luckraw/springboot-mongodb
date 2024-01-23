1d7f52680d10ee94213bba391bd1c5fdd44d624f
nulld <krawdev@gmail.com>
2024-01-23 12:12:11 -0300

    feat: Upload/Download Photo impl.

--- src/main/java/com/luckraw/springbootmongodb/service/PhotoService.java
+++ src/main/java/com/luckraw/springbootmongodb/service/PhotoService.java
@@ -1,4 +1,12 @@
 package com.luckraw.springbootmongodb.service;
 
+import com.luckraw.springbootmongodb.model.entity.Photo;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface PhotoService {
+    String addPhoto(String originalFilename, MultipartFile image) throws IOException;
+
+    Photo getPhoto(String id);
 }
